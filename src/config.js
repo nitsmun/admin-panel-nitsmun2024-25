f77b35493cfa576d76908460b07a1d89fda4d1a5
barnilsarma <barnilsarma@gmail.com>
2024-06-22 05:34:20 +0530

    firebase changes

--- src/config.js
+++ src/config.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDNzWMrngCNSJpFnAcN2i23X_YOgibwwto",
-    authDomain: "nitsmun-auth-2024-25.firebaseapp.com",
-    projectId: "nitsmun-auth-2024-25",
-    storageBucket: "nitsmun-auth-2024-25.appspot.com",
-    messagingSenderId: "680655237904",
-    appId: "1:680655237904:web:6c55e3d19393a276f7ab95",
-    measurementId: "G-60E4EYK30S"
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_STORAGE_BUCKET,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
